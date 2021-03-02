39505854ceb105596ffca080ca728670381199b6
Bruno Porceli Alaniz <porcelialaniz@gmail.com>
2021-03-02 18:54:10 -0300

    ok

--- Madlibs.py
+++ Madlibs.py
@@ -7,7 +7,7 @@ Prompts the user for works, then prints a story with those words.
 
 STORY = "Esta manhã %s acordou sentindo %s. 'Vai ser um %s dia!' Lá fora, vários %ss estavam protestando para manter %s nas lojas. Eles começaram a %s ao ritmo de %s, o que tornava todos os %ss muito %s. Preocupado, %s enviou uma mensagem de texto para %s, que voou %s para %s e caiu %s em uma poça de %s congelada. %s acordou no ano %s, em um mundo onde %ss governava o mundo."
 
-print 'MadLibs is starting!'
+print("MadLibs is starting!")
 
 name = raw_input('Enter a Name: ')
 
@@ -28,4 +28,4 @@ country = raw_input('Enter a country: ')
 dessert = raw_input('Enter a dessert: ')
 year = raw_input('Enter a year: ')
 
-print STORY % (name, adjective1, adjective2, animal, food, verb, noun1, frut, adjective3, name, superhero, name, country, name, dessert, name, year, noun2)
+print STORY%(name, adjective1, adjective2, animal, food, verb, noun1, frut, adjective3, name, superhero, name, country, name, dessert, name, year, noun2)
